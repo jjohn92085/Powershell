b4c47065cae2d58089efb3ac4dd7c90e0d94dfa7
jjohn92085 <jjohn92085@gmail.com>
2026-04-23 11:56:04 -0700

    updated

--- Service-Health/service-check-tool.ps1
+++ Service-Health/service-check-tool.ps1
@@ -1,6 +1,6 @@
 # this is a tool to check if specific services are running on startup and turn them off as needed
 
-# run as admin
+# run the script as admin
 
 (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
