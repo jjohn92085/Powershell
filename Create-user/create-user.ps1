20172b09eff0863221f72f5eb5008c0be9129ab4
jjohn92085 <jjohn92085@gmail.com>
2026-04-23 20:55:43 -0700

    create locatel

--- Create-user/create-user.ps1
+++ Create-user/create-user.ps1
@@ -1,1 +1,30 @@
 # create new user script
+
+#run script after logging in
+# -WindowStyle Hidden hides the visible console
+
+# powershell.exe -WindowStyle Hidden -File "%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"
+
+# ask user for username and password
+
+$Username = Read-Host "Enter new username"
+$Password = Read-Host "Enter password" -AsSecureString
+
+# create new user with a password prompt
+# New-LocalUser creates new local user accounts
+
+New-LocalUser `
+    -Name $UserName `
+    -Password $Password `
+    -Description "Local admin account"
+
+# add user to admin group
+
+Add-LocalGroupMember -Group "Administrators" -Member $UserName
+
+
+
+
+
+
+
