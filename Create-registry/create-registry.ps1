2a17a4684da46d2bb7b9eb1929d67df2b2fc041c
jjohn92085 <jjohn92085@gmail.com>
2026-04-26 01:01:31 -0700

    checks if path excists

--- Create-registry/create-registry.ps1
+++ Create-registry/create-registry.ps1
@@ -2,13 +2,18 @@
 
 Get-CimInstance Win32_ComputerSystem | Select-Object UserName
 
-#test is key already exists
+# test is key already exists and create it 
+# path says where to create the value
+# Name names the registry value
+# value puts the data in
 
 if (Test-Path -Path "HKCU:\Software\Endpoint-Tool") {
     Write-Host "the path already exists"
 } else {
     New-ItemProperty -Path "HKCU:\Software\Endpoint-Tool" -Name "endpoint-session-health-check-tool" -Value "1" -PropertyTypeString
 }
+
+
 
 
 
