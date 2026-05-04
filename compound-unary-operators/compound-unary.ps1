cb72ded619d9057127ff055d3bbaed8c20ec44a3
jjohn92085 <jjohn92085@gmail.com>
2026-05-04 16:18:46 -0700

    compound-unary

--- compound-unary-operators/compound-unary.ps1
+++ compound-unary-operators/compound-unary.ps1
@@ -1,1 +1,33 @@
 #test of compound and unary operators
+
+# multiply
+
+[int]$multiplyNumber = 2
+$multiplyNumber *= 3
+Write-Host $multiplyNumber
+
+#add
+
+$addNumber = [int]"5"
+$addNumber += 3
+Write-Host $addNumber
+
+#subtract
+
+$newNumber = 4
+$subtractNumber = 5
+$newNumber -= $subtractNumber
+Write-Host $newNumber
+
+#divide
+
+$divideNumber /= 10
+Write-Host $divideNumber
+
+#type cast int to string
+
+$numberToString = [string]55
+$numbertoString += 3
+Write-Host $numberToString
+
+
